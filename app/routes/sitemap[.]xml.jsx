44ec1856b858e646cfa96e3a378cb7edf6a6e58c
Serhii Mykhalskyi <serhii.mykhalskyi@gmail.com>
2025-02-19 13:56:22 +0200

    Added robots.txt and sitemap.xml

---
+++ app/routes/sitemap[.]xml.jsx
@@ -0,0 +1,20 @@
+export const loader = () => {
+  const content = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+    <loc>https://nymphaea.shop/</loc>
+    <lastmod>2025-02-19</lastmod>
+    <priority>1.0</priority>
+    </url>
+    </urlset>
+    `;
+
+  return new Response(content, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+      'xml-version': '1.0',
+      encoding: 'UTF-8',
+    },
+  });
+};
